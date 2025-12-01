bb9815b0d715ef9b0db167436db335df3215210c
sufiarh <sufiweb11@gmail.com>
2025-12-01 22:19:44 +0800

    Update remote-install.sh

--- remote-install.sh
+++ remote-install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
+
+echo "=== Archstudio Online Installer ==="
+
+sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm git
+
+if [ -d ~/archstudio ]; then
+    rm -rf ~/archstudio
+fi
+
 git clone https://github.com/sufiarh/archstudio.git ~/archstudio
 cd ~/archstudio
 chmod +x install.sh
+
 ./install.sh
